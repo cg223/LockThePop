9989462a67117e934e6e45ae5bdca3e939e27e9d
Caleb Griffis <117208697+cg223@users.noreply.github.com>
2023-05-09 10:44:26 -0500

    Work on gme

--- code.py
+++ code.py
@@ -8,10 +8,10 @@ stage = "STARTUP"
 target = 0
 isIncreasing = True
 count = 0
-interval = 0.75
+interval = 5000
 
-colorsDark = [0x220000, 0x221100, 0x222200, 0x112200,
-0x002200, 0x002211, 0x002222, 0x000022, 0x110022, 0x220011]
+colorsDark = [0x110000, 0x110800, 0x111100, 0x081100,
+0x001100, 0x001108, 0x001111, 0x000011, 0x080011, 0x110008]
 colorsLit = [0xFF0000, 0xFFA500, 0xFFFF00, 0xA5FF00,
 0x00FF00, 0x00FFA5, 0x00FFFF, 0x0000FF, 0x5500FF, 0xFF00A5]
 tones = [220, 261, 329, 392, 523, 587, 659, 698, 783, 880]
@@ -25,8 +25,8 @@ def setColor(index):
 def startup():
     for i in range(10):
         setColor(i)
-        cpx.play_tone(tones[i], 0.5)
-        sleep(0.5)
+        #cpx.play_tone(tones[i], 0.25)
+        sleep(0.25)
 
 def getNewTarget(currentLight):
     newTarget = currentLight
@@ -34,36 +34,43 @@ def getNewTarget(currentLight):
         newTarget = randrange(0, 10)
     return newTarget
 
+steps = 0
+
 while True:
-    if stage == "STARTUP":
-        startup()
-        stage = "SETTING TARGET"
-        
-    elif stage == "SETTING LEVEL":
-        target = getNewTarget(currentLight)
-        stage = "PLAYING"
+    steps += 1
+    if steps % interval == 0:
+        if stage == "STARTUP":
+            startup()
+            stage = "SETTING TARGET"
 
-    elif stage == "PLAYING":
-        for i in range(10):
-            if i == target:
-                cpx.pixels[i] = colorsLit[level-1]
-            elif i == currentLight:
-                cpx.pixels[i] = colorsLit[level]
+        elif stage == "SETTING TARGET":
+            target = getNewTarget(currentLight)
+            stage = "PLAYING"
+
+        elif stage == "PLAYING":
+            for i in range(10):
+                if i == target:
+                    cpx.pixels[i] = colorsLit[level-1]
+                elif i == currentLight:
+                    cpx.pixels[i] = colorsLit[level]
+                else:
+                    cpx.pixels[i] = colorsDark[level]
+
+            if isIncreasing:
+                currentLight += 1
             else:
-                cpx.pixels[i] = colorsDark[level]
+                currentLight -= 1
 
-        if isIncreasing:
-            currentLight += 1
-        else:
-            currentLight -= 1
+            if currentLight == -1:
+                currentLight = 9
+            elif currentLight == 10:
+                currentLight = 0
 
-        if currentLight == -1:
-            currentLight = 9
-        elif currentLight == 10:
-            currentLight = 0
-
+    if stage == "PLAYING":
         if cpx.button_a:
+            print('a_button')
             if currentLight == target:
+                print('correct')
                 stage = "SETTING LEVEL"
                 if isIncreasing:
                     isIncreasing = False
@@ -75,8 +82,7 @@ while True:
                     level += 1
                     count = 0
                     interval *= 0.9
-            
             else:
-                pass
-    sleep(interval)
+                continue
+            print()
 
