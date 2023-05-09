b961e0fc20757cbf7dba94d215273599792f2198
Caleb Griffis <117208697+cg223@users.noreply.github.com>
2023-05-09 11:08:13 -0500

    5/9/23

--- code.py
+++ code.py
@@ -10,10 +10,12 @@ isIncreasing = True
 count = 0
 interval = 5000
 
-colorsDark = [0x110000, 0x110800, 0x111100, 0x081100,
-0x001100, 0x001108, 0x001111, 0x000011, 0x080011, 0x110008]
+colorsDark = [0x100000, 0x100500, 0x101000, 0x051000,
+0x001000, 0x001005, 0x001010, 0x000010, 0x050010, 0x100005]
 colorsLit = [0xFF0000, 0xFFA500, 0xFFFF00, 0xA5FF00,
 0x00FF00, 0x00FFA5, 0x00FFFF, 0x0000FF, 0x5500FF, 0xFF00A5]
+colorsTarget = [0xFF7700, 0xFFFF00, 0x77FF00, 0x00FF00,
+0x00FF77, 0x00FFFF, 0x0077FF, 0x7700FF, 0xFF00FF, 0xFF0077]
 tones = [220, 261, 329, 392, 523, 587, 659, 698, 783, 880]
 
 cpx.pixels.brightness = 0.1
@@ -30,7 +32,7 @@ def startup():
 
 def getNewTarget(currentLight):
     newTarget = currentLight
-    while(newTarget == currentLight):
+    while(newTarget == currentLight or newTarget == 9 or newTarget == 0):
         newTarget = randrange(0, 10)
     return newTarget
 
@@ -50,7 +52,7 @@ while True:
         elif stage == "PLAYING":
             for i in range(10):
                 if i == target:
-                    cpx.pixels[i] = colorsLit[level-1]
+                    cpx.pixels[i] = colorsTarget[level]
                 elif i == currentLight:
                     cpx.pixels[i] = colorsLit[level]
                 else:
@@ -68,21 +70,22 @@ while True:
 
     if stage == "PLAYING":
         if cpx.button_a:
-            print('a_button')
-            if currentLight == target:
-                print('correct')
-                stage = "SETTING LEVEL"
-                if isIncreasing:
+            if isIncreasing:
+                if currentLight == target+1:
+                    target = getNewTarget(currentLight)
                     isIncreasing = False
-                else:
-                    isIncrease = True
-
-                count += 1
-                if count == 5:
-                    level += 1
-                    count = 0
-                    interval *= 0.9
+                    count += 1
+                    if count == 5:
+                        level += 1
+                        count = 0
+                        interval *= 0.9
             else:
-                continue
-            print()
+                if currentLight == target-1:
+                    target = getNewTarget(currentLight)
+                    isIncreasing = True
+                    count += 1
+                    if count == 5:
+                        level += 1
+                        count = 0
+                        interval *= 0.9
 
