904bc5b7abf2c88b7a01e63ff08db1415f1f3054
kazdenhi <132926931+kazdenhi@users.noreply.github.com>
2023-05-10 11:16:09 -0500

    changed intro to caleb cutie

--- code.py
+++ code.py
@@ -17,7 +17,7 @@ colorsLit = [0xFF0000, 0xFFA500, 0xFFFF00, 0xA5FF00,
 0x00FF00, 0x00FFA5, 0x00FFFF, 0x0000FF, 0x5500FF, 0xFF00A5]
 colorsTarget = [0xFF7700, 0xFFFF00, 0x77FF00, 0x00FF00,
 0x00FF77, 0x00FFFF, 0x0077FF, 0x7700FF, 0xFF00FF, 0xFF0077]
-tones = [220, 261, 329, 392, 440, 523, 587, 659, 698, 783, 880, 987]
+tones = [220, 261, 329, 392, 523, 587, 659, 698, 783, 880, ]
 
 cpx.pixels.brightness = 0.1
 
@@ -27,16 +27,9 @@ def setColor(index):
 
 def startup():
     duration = .5
-    red(duration)
-    orange(duration)
-    yellow(duration)
-    yellow2(duration)
-    green(duration)
-    green2(duration)
-    aqua(duration)
-    blue(duration)
-    indigo(duration)
-    violet(duration)
+    for i in range(10):
+        color(i, duration)
+
     for i in range(10):
         cpx.pixels[i] = colorsLit[0]
         cpx.play_tone(200, .01)
@@ -54,67 +47,12 @@ def startup():
         cpx.pixels[i] = 0x000000
     sleep(.1)
 
-def red(duration):
+def color(index, duration):
     for i in range(10):
-        cpx.pixels[i] = colorsLit[0]
-    cpx.play_tone(329, duration)
+        cpx.pixels[i] = colorsLit[index]
+    cpx.play_tone(tones[index], duration)
     for i in range(10):
-        cpx.pixels[i] = colorsDark[0]
-def orange(duration):
-    for i in range(10):
-        cpx.pixels[i] = colorsLit[1]
-    cpx.play_tone(392, duration)
-    for i in range(10):
-        cpx.pixels[i] = colorsDark[1]
-def yellow(duration):
-    for i in range(10):
-        cpx.pixels[i] = colorsLit[2]
-    cpx.play_tone(440, duration)
-    for i in range(10):
-        cpx.pixels[i] = colorsDark[2]
-def yellow2(duration):
-    for i in range(10):
-        cpx.pixels[i] = colorsLit[3]
-    cpx.play_tone(523, duration)
-    for i in range(10):
-        cpx.pixels[i] = colorsDark[3]
-def green(duration):
-    for i in range(10):
-        cpx.pixels[i] = colorsLit[4]
-    cpx.play_tone(587, duration)
-    for i in range(10):
-        cpx.pixels[i] = colorsDark[4]
-def green2(duration):
-    for i in range(10):
-        cpx.pixels[i] = colorsLit[5]
-    cpx.play_tone(659, duration)
-    for i in range(10):
-        cpx.pixels[i] = colorsDark[5]
-def aqua(duration):
-    for i in range(10):
-        cpx.pixels[i] = colorsLit[6]
-    cpx.play_tone(698, duration)
-    for i in range(10):
-        cpx.pixels[i] = colorsDark[6]
-def blue(duration):
-    for i in range(10):
-        cpx.pixels[i] = colorsLit[7]
-    cpx.play_tone(783, duration)
-    for i in range(10):
-        cpx.pixels[i] = colorsDark[7]
-def indigo(duration):
-    for i in range(10):
-        cpx.pixels[i] = colorsLit[8]
-    cpx.play_tone(880, duration)
-    for i in range(10):
-        cpx.pixels[i] = colorsDark[8]
-def violet(duration):
-    for i in range(10):
-        cpx.pixels[i] = colorsLit[9]
-    cpx.play_tone(987, duration)
-    for i in range(10):
-        cpx.pixels[i] = colorsDark[9]
-
+        cpx.pixels[i] = colorsDark[index]
 
 def getNewTarget(currentLight):
     newTarget = currentLight
